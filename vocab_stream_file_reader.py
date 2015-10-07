b7a48e98f25e7c02a0f26e803d25e7c9de663367
rustyBilges <tophermcw@gmail.com>
2015-10-07 20:51:24 +0100

    Audio_stream_file_recorder passing first ever test

--- vocab_stream_file_reader.py
+++ vocab_stream_file_reader.py
@@ -7,7 +7,19 @@ import unittest
 class Audio_stream_file_reader():
 	"""A class to read vocab from a saved .wav audio file"""
 	## inevitably this class should implement an interface for a generic stream_reader (as should e.g. Audio_stream_live_recorder())
+	
+	def __init__(self, fname=None):
 
+		self.fname = fname
+		
+	def check_file(self):
+	
+		try:
+			f = open(fname)
+			f.close()
+
+		except:
+			raise IOError('Cannot open audio stream file.')
 
 class Test_audio_stream_file_reader(unittest.TestCase):
 	"""Class to test functionality of Audio_stream_file_reader"""
@@ -15,7 +27,7 @@ class Test_audio_stream_file_reader(unittest.TestCase):
 	def test_file_open(self):
 		"""Check that handling of bogus fname is safe"""
 		asfr = Audio_stream_file_reader('bogus_fname.txt')
-		with self.assertRaises(ValueError):
+		with self.assertRaises(IOError):
 			asfr.check_file()
 
 
