f560e5cd0e7c22b0f6e1b71b72bcf6bfb88d7a83
rustyBilges <tophermcw@gmail.com>
2015-10-07 21:44:04 +0100

    File name change

--- audio_streaming.py
+++ audio_streaming.py
@@ -9,13 +9,14 @@ class Audio_stream_const():
 	"""A class to represent a fixed stream of audio data.
 		ToDo: have this class implement a stream interface. Later to write e.g. Audio_stream_dynamic
 	"""  
-	def __init__(self, length=None, frequency=None):
+	def __init__(self, data, frequency=None):
 
-		self.length = length
+		self.data = data
+		self.length = len(data)
 		self.frequency = frequency	
 
-class Audio_stream_file_reader():
-	"""A class to read vocab from a saved numpy array representing an audio stream
+class Audio_stream_csv_reader_mono():
+	"""A class to read vocab from a saved numpy array (csv) representing an audio stream
 	   For reading of other file types (e.g. .wav) - Implement new classes!	
 	 
 		ToDo: this class should implement an interface for a generic stream_reader 
@@ -33,31 +34,34 @@ class Audio_stream_file_reader():
 			raise IOError('Cannot open audio stream file.')
 
 	def read(self):
-
-		return Audio_stream_const()
+		data = np.genfromtxt(self.fname, delimiter=',')
+		return Audio_stream_const(data)
 
 
 ##############################################################################################################
 ## From here on is unit test code for the above defined classes:
 
-class Test_audio_stream_file_reader(unittest.TestCase):
-	"""Class to test functionality of Audio_stream_file_reader
+class Test_audio_stream_csv_reader_mono(unittest.TestCase):
+	"""Class to test functionality of Audio_stream_csv_reader_mono
 
 	Note that a valid audio stream file is requried for tests to succeed.
-	This should be called 'raw_vocab.data'
+	This should be called 'raw_vocab.data', and have following properties:
+		> shape (88240,)
+		> comma separated
+		> mono i.e. just a list of wave amplitude values
 	"""
 
 	def test_file_open(self):
 		"""Check that handling of bogus and correct file names is safe"""
 		with self.assertRaises(IOError):
-			asfr = Audio_stream_file_reader('bogus_fname.txt')
+			asfr = Audio_stream_csv_reader_mono('bogus_fname.txt')
 
 
 	def test_return_stream(self):
-		"""Check that the read method returns an audio stream object"""
-		asfr = Audio_stream_file_reader('raw_vocab.data')
+		"""Check that the read method returns an audio stream object, with correct attribute values"""
+		asfr = Audio_stream_csv_reader_mono('raw_vocab.data')
 		self.assertIsInstance(asfr.read(), Audio_stream_const)    ## maybe this will need to change to Audio_stream (dynamic or const)
-		
+		self.assertEqual(asfr.read().length, 88240)
 
 if __name__=='__main__':
 
